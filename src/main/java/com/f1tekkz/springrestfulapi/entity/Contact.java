b82547d7c8c9ca71bad2168428162654fc6a117f
afwanz <afwanzikri@gmail.com>
2023-07-05 16:37:35 +0700

    add email validation on contact entity

--- src/main/java/com/f1tekkz/springrestfulapi/entity/Contact.java
+++ src/main/java/com/f1tekkz/springrestfulapi/entity/Contact.java
@@ -1,6 +1,8 @@
 package com.f1tekkz.springrestfulapi.entity;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -26,6 +28,9 @@ public class Contact {
 
     private String phone;
 
+    @Email(message = "Email is not valid",
+            regexp = "[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,3}",
+            flags = Pattern.Flag.CASE_INSENSITIVE)
     private String email;
 
     @Column(name = "created_by")
